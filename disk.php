9ea38caf0b78ecc632e25c38977feb7c5435daf8
Evan Moreau <moreeva@ndlp.fr>
2023-11-10 16:48:13 +0100

    patch_cd_menu

--- disk.php
+++ disk.php
@@ -7,7 +7,7 @@ $_PARAMS = $_POST;
 
 
 $CMD = $_PARAMS['CMD'];
-
+$pwd = null;
 function SIZE($location) {
     $size = filesize($location);
     $fileSize = 0;
@@ -20,18 +20,14 @@ function SIZE($location) {
     }
     return $fileSize;
 }
-
 function LOGIN(){
-    global $_PARAMS;
+    global $_PARAMS, $pwd; 
 
-    // Récupérer les paramètres de la commande
     $param1 = isset($_PARAMS['PARAM1']) ? $_PARAMS['PARAM1'] : '';
     $param2 = isset($_PARAMS['PARAM2']) ? $_PARAMS['PARAM2'] : '';
     error_log(' Username 1.1: ' . $param1);
 
-    // Vérifier si les paramètres requis sont présents
     if (!empty($param1) && !empty($param2)) {
-        // Effectuer les opérations de connexion
         $_SESSION['username'] = $param1;
         $_SESSION['mdp'] = $param2;
         error_log(' Username 1: ' . $param1);
@@ -45,19 +41,20 @@ function LOGIN(){
     } else {
         $response = array('status' => 'error', 'message' => 'Missing parameters for login');
     }
-
-    // Envoyer la réponse JSON
+    $pwd = '/home/'.$_SESSION['username'];
     send_response('LOGIN', $response['status'], $response['message']);
-    exit; // Assurez-vous de terminer l'exécution du script après avoir envoyé la réponse JSON
+    exit; 
 }
 function LOGOUT(){
+    global $pwd;
+
+    session_unset();
     session_destroy();
 
-    // Envoyer une réponse de déconnexion au format JSON
+    $pwd = null;
     send_response('LOGOUT', 'success', 'Logout successful');
     exit;
 }
-
 function WHOAMI(){
     if(isset($_SESSION['username'])) {
         error_log(' Username 2: ' . $_SESSION['username']);
@@ -69,7 +66,6 @@ function WHOAMI(){
     send_response('WHOAMI', $response['status'], $response['message']);
     exit;
 }
-
 function DIRECTORY() {
     if(isset($_SESSION['username'])) {
         $rep = 'users/' . $_SESSION['username'];
@@ -77,14 +73,14 @@ function DIRECTORY() {
         $response = array('status' => 'success', 'directories' => array());
 
         foreach ($dirs as $dir) {
-            if ($dir != '.' && $dir != '..') {
+            // if ($dir != '.' && $dir != '..') {
                 $location = $rep . '/' . $dir;
                 if (is_dir($location)) {
                     $response['directories'][] = array('type' => 'dir', 'name' => $dir);
                 } elseif(is_file($location)) {
                     $response['directories'][] = array('type' => 'file', 'name' => $dir, 'size' => SIZE($location), 'date' => filemtime($location));
                 }
-            }
+            // }
         }
 
         // Envoyer une réponse au format JSON
@@ -96,12 +92,45 @@ function DIRECTORY() {
         exit;
     }
 }
-
 function send_response($cmd, $status, $content = null){
-    $response = array('cmd' => $cmd, 'status' => $status, 'content' => $content,'debug' => array('user' => $_SESSION['username'], 'home' => $home, 'pwd' => $pwd ));
-    // Envoyer la réponse JSON
+    global $pwd;
+
+    $username = isset($_SESSION['username']) ? $_SESSION['username'] : 'N/A';
+
+    $response = array(
+        'cmd' => $cmd,
+        'status' => $status,
+        'content' => $content,
+        'debug' => array(
+            'user' => $username,
+            'home' => isset($_SESSION['username']) ? '/home/' . $_SESSION['username'] : 'N/A',
+            'pwd' => $pwd
+        )
+    );
+
     echo json_encode($response);
-    exit; // Assurez-vous de terminer l'exécution du script après avoir envoyé la réponse JSON
+    exit;
+}
+function LIST_DIRECTORIES() {
+    if (isset($_SESSION['username'])) {
+        $currentDirectory = 'users/' . $_SESSION['username'];
+        $dirs = scandir($currentDirectory);
+        $directories = array();
+
+        foreach ($dirs as $dir) {
+            if ($dir != '.' && $dir != '..' && is_dir($currentDirectory . '/' . $dir)) {
+                $directories[] = $dir;
+            }
+        }
+
+        $response = array('status' => 'success', 'directories' => $directories);
+        send_response('LIST_DIRECTORIES', $response['status'], $response['directories']);
+        exit;
+    } else {
+        $response = array('status' => 'error', 'message' => 'User not logged in');
+        send_response('LIST_DIRECTORIES', $response['status'], $response['message']);
+        exit;
+    }
 }
 
 
@@ -110,6 +139,7 @@ switch($CMD) {
     case "LOGOUT": LOGOUT(); break;
     case "WHOAMI": WHOAMI(); break;
     case "DIR": DIRECTORY(); break;
+    case "CD1": LIST_DIRECTORIES(); break;
     default:
     // Envoyer une réponse d'erreur au format JSON pour les commandes non reconnues
     $response = array('status' => 'error', 'message' => 'Unknown command');
